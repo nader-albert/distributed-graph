a858c59de4b381e2e3608eb73022b50dc5e10c3a
Nader Albert <nader.albert@gmail.com>
2017-06-13 22:47:02 +1000

    added Builder to initialise the application

---
+++ src/main/scala/na/distributedGraph/app/GraphBuilder.scala
@@ -0,0 +1,46 @@
+package na.distributedGraph.app
+
+import akka.actor.{ActorSystem, Props}
+import com.typesafe.config.ConfigFactory
+import na.distributedGraph.commands.corporates.Add
+import na.distributedGraph.entities.businesses.{BusinessGuardian, Employer}
+import na.distributedGraph.entities.persons.{Person, PersonsGuardian}
+import scala.language.postfixOps
+
+
+import scala.util.Random
+
+object GraphBuilder extends App {
+
+    val system = ActorSystem("DistributedGraph")
+
+    val config = ConfigFactory load
+
+    val applicationConfig = config getConfig "graph"
+
+    val businessesConfig = applicationConfig getConfig "business_config"
+    val personsConfig = applicationConfig getConfig "person_config"
+
+    println("******************** Initializing graph data structure **************************")
+
+    val businesses = system.actorOf(BusinessGuardian.props(businessesConfig), name = "businesses-guardian")
+    println("\r\n ************************** root corporates node initialised ************************** \r\n" )
+
+    val persons = system.actorOf(PersonsGuardian.props(personsConfig), name = "persons-guardian")
+    println("\r\n ************************** Root Persons Node Initialised ************************** \r\n" )
+
+
+    println("\r\n ************************** adding 20 different corporates ************************** \r\n")
+    (1 to 20).foreach { index =>
+        val business = system.actorOf(Employer.props(index), "corporate-" + index)
+        businesses ! Add(business)
+    }
+
+    println("\r\n ************************** adding 20 different persons ************************** \r\n")
+
+    (1 to 20).foreach { index =>
+        val person = system.actorOf(Person.props(index), "person-" + index)
+        persons ! Add(person)
+    }
+
+}
