7634429e2c7e21ab92b0e04b8e6cfeb04c3d1ba5
Nader Albert <nader.albert@gmail.com>
2017-06-14 12:33:58 +1000

    package restructuring- continued

--- src/main/scala/na/distributedGraph/events/CorporateEvent.scala
+++
@@ -1,6 +0,0 @@
-package na.distributedGraph.events
-
-trait CorporateEvent
-
-case object Joined extends CorporateEvent
-case object Left extends CorporateEvent
--- src/main/scala/na/distributedGraph/events/RelationshipEvent.scala
+++
@@ -1,7 +0,0 @@
-package na.distributedGraph.events
-
-trait RelationshipEvent
-
-case object FriendRequestAccepted extends RelationshipEvent
-
-case object RelationAccepted extends RelationshipEvent
